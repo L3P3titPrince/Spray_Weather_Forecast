db8d691e7c105b488c58f00b99501c17ca38316a
L3P3titPrince <l3.p3tit.prince@gmail.com>
2021-03-04 13:03:46 -0500

    Mar4th 2021 add read_csv() function

--- class_32_import_data.py
+++ class_32_import_data.py
@@ -1,4 +1,4 @@
-from class_31_hyperparameters import HyperParamters
+# from class_31_hyperparameters import HyperParamters
 
 # read xlxs file
 import pandas as pd
@@ -38,22 +38,37 @@ class ImportData(HyperParamters):
         start_time = time()
 
         # create excel full paht directory
-        excel_product = os.path.join(self.ROOTPATH, '017_20160101_20201231_ProductionData.xlsx')
+        excel_product = os.path.join(self.ROOTPATH, '019_20160101_20201231_ProductionData.xlsx')
         # first sheet is real production data
         df_product = pd.read_excel(excel_product, sheet_name = '20160101_20201231_ProductionDat')
         # second sheet is NJ weather data
         df_nj_weather = pd.read_excel(excel_product, sheet_name = 'MiddlesexWeather')
         # third sheet is PA weather data
         df_pa_weather = pd.read_excel(excel_product, sheet_name = 'BethlehemWeather')
+        # this new data missing rate, so we have to import old data and merge ['Rate'] to our new data
+        old_product = os.path.join(self.ROOTPATH, '017_20160101_20201231_ProductionData.xlsx')
+        df_old = pd.read_excel(old_product, sheet_name = '20160101_20201231_ProductionDat')
+        df_product['Rate'] = df_old['Rate']
+
+        # save
+        df_product.to_csv('03_data/20_production.csv',index=False)
+        df_nj_weather.to_csv('03_data/21_nj_weather.csv', index=False)
+        df_pa_weather.to_csv('03_data/22_pa_weather.csv', index=False)
 
         cost_time = round((time() - start_time), 4)
         print("*" * 40, "End import_data() with {} second".format(cost_time), "*" * 40, end='\n\n')
 
         return df_product, df_nj_weather, df_pa_weather
 
+    def read_csv(self):
+        """":arg
+        Because reading excel is too slow, so we use this function to read csv that processed and saved by
+        import_data() function.
+        """
+        df_product = pd.read_csv('03_data/20_production.csv')
+        df_nj_weather = pd.read_csv('03_data/21_nj_weather.csv')
+        df_pa_weather = pd.read_csv('03_data/22_pa_weather.csv')
 
 
-
-
-
+        return df_product, df_nj_weather, df_pa_weather
 
