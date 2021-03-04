38e02329975d88e209ad96f6ab419da31574c2e9
L3P3titPrince <l3.p3tit.prince@gmail.com>
2021-03-04 12:56:46 -0500

    Mar4th 2021 Add 33_eda

---
+++ class_33_eda.py
@@ -0,0 +1,61 @@
+from class_31_hyperparameters import HyperParamters
+
+# used for visulazation
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+class EDA(HyperParamters):
+    """:arg
+    """
+    def __init__(self):
+        """:arg
+        """
+        HyperParamters.__init__(self)
+
+    def missing_plot(self, df: pd.DataFrame):
+        """:arg
+
+        Used for missing data plot for count plot and percenatage plot
+        """
+        # not all columns contain missing value, we only plot the colunms have missing value
+        # checking is there any missing value in this df and find threir column names and assign to col_missing_value
+        col_missing_value = list(df.columns[df.isnull().any()])
+        # count the missing value for each column
+        df[col_missing_value].isnull().sum()
+        # to hold the columns names
+        list_labels=[]
+        # to hold the count of missing values for each variable
+        list_value_count=[]
+        # to hold the percentage of missing values for each variable
+        list_percent_count=[]
+        #
+        for col in col_missing_value:
+            # add the column names into this list
+            list_labels.append(col)
+            # caculate each column missing value count and append to list
+            list_value_count.append(df[col].isnull().sum())
+            # df.shape[0] will give totoal row count
+            list_percent_count.append(df[col].isnull().sum()/df.shape[0])
+
+
+        # create two subplot with 1 row two columns
+        fig, (ax1, ax2) = plt.subplots(1,2)
+        # we use ax.barh() create horizontal bar chart, bar width is the count number of missing value
+        ax1.barh(y = np.arange(len(list_labels)),width = np.array(list_value_count), height=0.5, color='blue')
+        # set lenght of y labels
+        ax1.set_yticks(np.arange(len(list_labels)))
+        # set y labels
+        ax1.set_yticklabels(list_labels, rotation='horizontal')
+        ax1.set_xlabel('Count of missing values')
+        ax1.set_title("Columns with missing value count")
+
+        ax2.barh(y=np.arange(len(list_labels)), width = np.array(list_percent_count), height=0.5, color='red')
+        ax2.set_yticks(np.arange(len(list_labels)))
+        ax2.set_yticklabels(list_labels, rotation='horizontal')
+        ax2.set_xlabel("Percentage of missing values")
+        ax2.set_title("Columns with missing values")
+
+        plt.show()
+        print(col_missing_value, type(col_missing_value))
+        return col_missing_value
