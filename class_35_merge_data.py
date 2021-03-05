6271dd2e9e7d59de3a19c5855c4f40a637f0fab3
L3P3titPrince <l3.p3tit.prince@gmail.com>
2021-03-04 22:43:26 -0500

    Mar 4th 2021 change name

---
+++ class_35_merge_data.py
@@ -0,0 +1,27 @@
+from class_31_hyperparameters import HyperParamters
+
+from pandas import pd
+
+class MergeData(HyperParamters):
+    """:arg
+    For now, we have differenty hypothesis to view our data
+    We will use self.MERGE_WAY to choose different hypothesis
+
+    1.self.MERGE_WAY:one
+        without further process, just using dt_est to merge
+    2.self.MERGE_WAY:
+
+    """
+    def __init__(self):
+        HyperParamters.__init__(self)
+
+
+
+    def merge_one(self, df_product, df_nj_weather, df_pa_weather):
+        """:arg
+        Don't split, just merge all together by ['dt_est']
+        """
+        df_nj = pd.merge(df_product, df_nj_weather, how='left', on=['dt_est'])
+        df_pa = pd.merge(df_product, df_pa_weather, how='left', on=['dt_est'])
+
+        return df_nj, df_pa
